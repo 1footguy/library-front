78338cb2b0f71174d8365c3f7d4ac6163eaefe17
Jonatas D. Assis <jonatasdeassis@gmail.com>
2024-12-09 21:10:57 -0300

    feat: adicionada chamada à minha API REST para adicionar/carregar/atualizar/deletar livros

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,96 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react"
+import { useState } from "react"
+import { useForm } from "react-hook-form";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [livros, setLivros] = useState([]);
+  const {register, handleSubmit} = useForm();
+
+  async function salvar(data){
+    const {titulo, autor, paginas} = data;
+    await fetch("http://localhost:3000/livros", {
+      method: "POST",
+      headers: { "Content-Type" : "application/json"},
+      body: JSON.stringify({titulo, autor, paginas})
+    });
+
+  }
+
+  async function loadData() {
+    const response = await fetch("http://localhost:3000/livros");
+    const data = await response.json();
+      setLivros(data);
+    loadData();
+
+  }
+
+  async function excluir(id) {
+    await fetch(`http://localhost:3000/livros/${id}`, {
+      method: "DELETE"
+    })
+    loadData();
+  }
+
+  async function editar(id) {
+    const titulo = window.prompt("Digite o novo título")
+    await fetch (`http://localhost:3000/livros/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type" : "application/json"},
+      body: JSON.stringify({titulo})
+    })
+    loadData();
+  }
+
+  useEffect(() => {
+    loadData();
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <form onSubmit={handleSubmit(salvar)}>
+        <label htmlFor="titulo">Titulo</label>
+        <input type="text" id="titulo" {...register("titulo")}/>
+        
+        <label htmlFor="autor">Autor</label>
+        <input type="text" id="autor" {...register("autor")} />
+        
+        <label htmlFor="paginas">Qtd. Paginas</label>
+        <input type="number" id="paginas" {...register("paginas")}/>
+
+        <button type="submit">Salvar</button>
+      </form>
+
+      <h1>Livros</h1>
+      <table>
+        <thead>
+          <tr>
+            <td>Titulo</td>
+            <td>Autor</td>
+            <td>Categoria</td>
+            <td>Qtd pag.</td>
+          </tr>
+        </thead>
+        <tbody>
+          {livros.map(livro => {
+            return(
+            <tr key={livro.id}>
+              <td>{livro.titulo}</td>
+              <td>{livro.autor}</td>
+              <td>{livro.categoria}</td>
+              <td>{livro.paginas}</td>
+              <td>
+                <button type="button" onClick={() => editar(livro.id)}> Editar </button>
+              </td>
+              <td>
+                <button type="button" onClick={() => excluir(livro.id)}> Excluir </button>
+              </td>
+            </tr>
+            )
+          })}
+
+        </tbody>
+      </table>
+    </div>
   )
 }
 
